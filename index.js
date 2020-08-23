3a014c3e011d42e652cef7d4f2c5e900c68133a8
ithiris <ithirisshabi@gmail.com>
2020-08-23 09:38:45 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,39 +1,54 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write Javascript code!
-function makeItDivResizable(div){
-var element = document.querySelector(div);
-const resizers = document.querySelectorAll(div + ' .corner')
-let currentResizer;
-for(let resizer of resizers ){
-resizer.addEventListener('mousedown',mouseDown);
-
-  function mouseDown(e){
-    currentResizer=e.target;
-   let mouse_x =e.pageX;
-   let mouse_y =e.pageY;
-    window.addEventListener('mousemove',resize);
-    window.addEventListener('mouseup',stopResize);
-
-      function resize(e){
-        const rectangle =element.getBoundingClientRect();
-       if (currentResizer.classList.contains('resizer-bottom-right')) {
-       element.style.width =rectangle.width -(mouse_x -e.pageX) +"px";
-       element.style.height =rectangle.height -(mouse_y -e.pageY) +"px";
-       }
-        mouse_x =e.pageX;
-       mouse_y =e.pageY;
+function makeItDivResizable(div) {
+  var element = document.querySelector(div);
+  const resizers = document.querySelectorAll(div + " .corner");
+  let currentResizer;
+  let isResizing =false;
+  for (let resizer of resizers) {
+    resizer.addEventListener("mousedown", mouseDown);
+    function mouseDown(e) {
+      currentResizer = e.target;
+      isResizing =true;
+      let mouse_x = e.pageX;
+      let mouse_y = e.pageY;
+      window.addEventListener("mousemove", resize);
+      window.addEventListener("mouseup", stopResize);
+      function resize(e) {
+        const rectangle = element.getBoundingClientRect();
+        if (currentResizer.classList.contains("resizer-bottom-right")) {
+          element.style.width = rectangle.width - (mouse_x - e.pageX) + "px";
+          element.style.height = rectangle.height - (mouse_y - e.pageY) + "px";
+        }
+       else if (currentResizer.classList.contains("resizer-bottom-left")) {
+          element.style.width = rectangle.width - (mouse_x - e.pageX) + "px";
+          element.style.height = rectangle.height - (mouse_y - e.pageY) + "px";
+           element.style.left = rectangle.left - (mouse_y - e.pageY) + "px";
+        }
+        else if (currentResizer.classList.contains("resizer-top-right")) {
+          element.style.width = rectangle.width - (mouse_x - e.pageX) + "px";
+          element.style.height = rectangle.height - (mouse_y - e.pageY) + "px";
+           element.style.top = rectangle.top - (mouse_y - e.pageY) + "px";
+        }
+        else{
+           element.style.width = rectangle.width - (mouse_x - e.pageX) + "px";
+          element.style.height = rectangle.height - (mouse_y - e.pageY) + "px";
+           element.style.top = rectangle.top - (mouse_y - e.pageY) + "px"
+           element.style.left = rectangle.left - (mouse_y - e.pageY) + "px";
+        }
+        mouse_x = e.pageX;
+        mouse_y = e.pageY;
       }
-  function stopResize(e){
-        e.preventDefault();
-    
+      function stopResize() {
+        window.removeEventListener("mousemove", resize);
+         window.removeEventListener("mousemove", stopResize);
+         isResizing =false;
+       
       }
-
+    }
   }
-
-
-}
 }
 
-const result =makeItDivResizable('.resizable')
+const result = makeItDivResizable(".resizable");
